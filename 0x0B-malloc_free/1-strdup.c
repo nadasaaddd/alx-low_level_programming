779cfaaaffb16f2a32530970aa7cb3a8cc5a3149
Nada <nadasaaddd@gmail.com>
2023-04-12 18:47:13 -0700

    edit task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,48 +2,32 @@
 #include "main.h"
 
 /**
- * *str_concat - concatenates two strings
- * @s1: string to concatenate
- * @s2: other string to concatenate
+ * *_strdup - copies the string given as parameter
+ * @str: string to duplicate
  *
- * Return: pointer to the new string created (Success), or NULL (Error)
+ * Return: pointer to the copied string (Success), NULL (Error)
  */
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
-	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
-
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
-
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
-		return (NULL);
+	char *dup;
+	unsigned int i, len;
 
 	i = 0;
-	j = 0;
+	len = 0;
 
-	if (s1)
-	{
-		while (i < len1)
-		{
-			s3[i] = s1[i];
-			i++;
-		}
-	}
+	if (str == NULL)
+		return (NULL);
 
-	if (s2)
-	{
-		while (i < (len1 + len2))
-		{
-			s3[i] = s2[j];
-			i++;
-			j++;
-		}
-	}
-	s3[i] = '\0';
+	while (str[len])
+		len++;
 
-	return (s3);
+	dup = malloc(sizeof(char) * (len + 1));
+
+	if (dup == NULL)
+		return (NULL);
+
+	while ((dup[i] = str[i]) != '\0')
+		i++;
+
+	return (dup);
 }
